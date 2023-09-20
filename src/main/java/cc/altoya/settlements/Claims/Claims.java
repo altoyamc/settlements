0e44c061ff158249c73e2dd3d3af3e8510f417b0
westshae <shaewest02@gmail.com>
2023-09-20 13:22:33 +1200

    untrust to claim

--- src/main/java/cc/altoya/settlements/Claims/Claims.java
+++ src/main/java/cc/altoya/settlements/Claims/Claims.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.List;
 import java.util.UUID;
 
 import org.bukkit.Bukkit;
@@ -149,7 +150,6 @@ public class Claims implements CommandExecutor {
       return false;
     }
 
-
     String targetName = args[1];
     OfflinePlayer targetPlayer = Bukkit.getOfflinePlayer(targetName);
 
@@ -209,6 +209,67 @@ public class Claims implements CommandExecutor {
     }
     if (args.length != 2) {
       return false;
+    }
+
+    String targetName = args[1];
+    OfflinePlayer targetPlayer = Bukkit.getOfflinePlayer(targetName);
+
+    if (targetPlayer == null) {
+      player.sendMessage("This player couldn't be found.");
+      return false;
+    }
+
+    UUID targetUUID = targetPlayer.getUniqueId();
+
+    String uuid = player.getUniqueId().toString();
+    int x = player.getLocation().getChunk().getX();
+    int y = player.getLocation().getChunk().getZ();
+
+    String query = "SELECT * FROM claims WHERE x = ? AND y = ?";
+
+    try {
+      PreparedStatement selectStatement = DatabaseConnections.getConnection().prepareStatement(query);
+      selectStatement.setInt(1, x);
+      selectStatement.setInt(2, y);
+      ResultSet resultSet = selectStatement.executeQuery();
+
+      if (resultSet.next()) {
+        String existingTrusted = resultSet.getString("trusted").replaceAll("}", "");
+
+        if (!existingTrusted.contains(targetUUID.toString())) {
+          player.sendMessage("This person isn't trusted.");
+          return false;
+        }
+
+        String[] trusted = existingTrusted.toString().split(",");
+
+        String trustedTotal = "{" + trusted[0];
+
+        for (int i = 1; i < trusted.length; i++) {
+          if (!trusted[i].equals(targetUUID.toString())) {
+            trustedTotal += "," + trusted[i];
+          }
+        }
+        trustedTotal += "}";
+
+        String updateQuery = "UPDATE claims SET trusted = ? WHERE x = ? AND y = ? AND uuid = ?";
+        PreparedStatement updateStatement = DatabaseConnections.getConnection().prepareStatement(updateQuery);
+        updateStatement.setString(1, trustedTotal);
+        updateStatement.setInt(2, x);
+        updateStatement.setInt(3, y);
+        updateStatement.setString(4, uuid);
+
+        int rowsUpdated = updateStatement.executeUpdate();
+
+        if (rowsUpdated > 0) {
+          player.sendMessage("Player has been untrusted successfully.");
+        } else {
+          player.sendMessage("Untrusting player has failed.");
+        }
+      }
+
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
 
     return true;
