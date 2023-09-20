afaa433a619b515df66b930f20f7f2597f84bd3e
westshae <shaewest02@gmail.com>
2023-09-20 12:48:28 +1200

    trust to claims

--- src/main/java/cc/altoya/settlements/Claims/Claims.java
+++ src/main/java/cc/altoya/settlements/Claims/Claims.java
@@ -1,9 +1,13 @@
 package cc.altoya.settlements.Claims;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
+import java.util.UUID;
 
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -143,6 +147,57 @@ public class Claims implements CommandExecutor {
     }
     if (args.length != 2) {
       return false;
+    }
+
+
+    String targetName = args[1];
+    OfflinePlayer targetPlayer = Bukkit.getOfflinePlayer(targetName);
+
+    if (targetPlayer == null) {
+      player.sendMessage("This player couldn't be found.");
+      return false;
+    }
+
+    UUID targetUUID = targetPlayer.getUniqueId();
+
+    String uuid = player.getUniqueId().toString();
+    int x = player.getLocation().getChunk().getX();
+    int y = player.getLocation().getChunk().getZ();
+
+    String query = "SELECT * FROM claims WHERE x = ? AND y = ?";
+    try {
+      PreparedStatement selectStatement = DatabaseConnections.getConnection().prepareStatement(query);
+      selectStatement.setInt(1, x);
+      selectStatement.setInt(2, y);
+      ResultSet resultSet = selectStatement.executeQuery();
+
+      if (resultSet.next()) {
+        String existingTrusted = resultSet.getString("trusted").replaceAll("}", "");
+
+        if (existingTrusted.contains(targetUUID.toString())) {
+          player.sendMessage("This person is already trusted.");
+          return false;
+        }
+
+        existingTrusted = existingTrusted + "," + targetUUID + "}";
+
+        String updateQuery = "UPDATE claims SET trusted = ? WHERE x = ? AND y = ? AND uuid = ?";
+        PreparedStatement updateStatement = DatabaseConnections.getConnection().prepareStatement(updateQuery);
+        updateStatement.setString(1, existingTrusted);
+        updateStatement.setInt(2, x);
+        updateStatement.setInt(3, y);
+        updateStatement.setString(4, uuid);
+
+        int rowsUpdated = updateStatement.executeUpdate();
+
+        if (rowsUpdated > 0) {
+          player.sendMessage("Player has been trusted successfully.");
+        } else {
+          player.sendMessage("Trusting player has failed.");
+        }
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
     }
 
     return true;
