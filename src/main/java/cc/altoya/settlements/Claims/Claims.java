7fe68b2164d4ea1a55541819b911c802e928296c
westshae <shaewest02@gmail.com>
2023-09-20 15:12:29 +1200

    chunk list basics

--- src/main/java/cc/altoya/settlements/Claims/Claims.java
+++ src/main/java/cc/altoya/settlements/Claims/Claims.java
@@ -283,6 +283,36 @@ public class Claims implements CommandExecutor {
       return false;
     }
 
+    String uuid = player.getUniqueId().toString();
+
+    String query = "SELECT * FROM claims WHERE uuid = ?";
+    try {
+      PreparedStatement selectStatement = DatabaseConnections.getConnection().prepareStatement(query);
+      selectStatement.setString(1, uuid);
+      ResultSet resultSet = selectStatement.executeQuery();
+
+      player.sendMessage("Test");
+
+      while (resultSet.next()) {
+        String[] existingTrusted = resultSet.getString("trusted").replaceAll("[{}]", "").split(",");
+        String names = "";
+        for(String trusted : existingTrusted){
+          names += Bukkit.getOfflinePlayer(UUID.fromString(trusted)).getName() + ",";
+        }
+        names = names.substring(0, names.length() - 1);
+
+        int x = resultSet.getInt("x");
+        int y = resultSet.getInt("y");
+
+        player.sendMessage(x + ":" + y + ":" + names);
+      }
+
+      player.sendMessage("Test");
+
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
     return true;
   }
 
