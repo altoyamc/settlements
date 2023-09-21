1052ae57a3e095f760f7acb41791d2ad7ab855c7
westshae <shaewest02@gmail.com>
2023-09-21 20:21:52 +1200

    changed CommandNew to 2 args

--- src/main/java/cc/altoya/settlements/Commands/Settlement/CommandNew.java
+++ src/main/java/cc/altoya/settlements/Commands/Settlement/CommandNew.java
@@ -10,8 +10,8 @@ public class CommandNew {
       ChatUtil.sendErrorMessage(player, "You don't have permission to run this command.");
       return true;
     }
-    if (args.length != 1) {
-      ChatUtil.sendErrorMessage(player, "This command only requires one argument. /settlement new");
+    if (args.length != 2) {
+      ChatUtil.sendErrorMessage(player, "This command only requires two argument. /settlement new {settlement-name}");
       return true;
     }
 
