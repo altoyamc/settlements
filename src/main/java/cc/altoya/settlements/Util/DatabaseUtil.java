df2db97328c8f35d4c507f42a74fe433628e5550
westshae <shaewest02@gmail.com>
2023-10-04 13:22:23 +1300

    removed unneeded imports

--- src/main/java/cc/altoya/settlements/Util/DatabaseUtil.java
+++ src/main/java/cc/altoya/settlements/Util/DatabaseUtil.java
@@ -5,9 +5,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
